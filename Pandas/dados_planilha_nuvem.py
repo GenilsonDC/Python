4a57f914e3f256d770d12c825ca81a3332fed934
Genilson do Carmo <57676824+GenilsonDC@users.noreply.github.com>
2023-08-31 17:17:00 -0300

    🐍 - Data sciense - Acesso a planilha na nuvem
    Acesso a dados na planilha que estava no Google Drive.

--- Pandas/dados_planilha_nuvem.py
+++ Pandas/dados_planilha_nuvem.py
@@ -1,1 +1,38 @@
-url = "https://docs.google.com/spreadsheets/d/1EM5SfxG8Fzg3f1GOMHzYkmNc7HdkoXY4XUyuW3TekxE/edit?usp=sharing"
+import pandas as pd
+import requests
+import io
+
+def main():
+    try:
+        # URL do arquivo na nuvem
+        url = "https://drive.google.com/uc?id=1ynn2dtqFwQJt3ruH761WAt1ftEgoQ5YP"
+        # CABECALHO: Cliente;Porduto;Valor unit R$;Quantidade
+        # Obter o conteúdo do arquivo da URL
+        response = requests.get(url)
+        content = response.content.decode('utf-8')
+
+        # Criar um objeto io.StringIO para simular um arquivo
+        file_like = io.StringIO(content)
+
+        # Lê o arquivo usando o pandas
+        arquivo_nuvem = pd.read_csv(file_like, delimiter=';')
+
+        # Converte a coluna "Quantidade" para valores numéricos
+        arquivo_nuvem["Quantidade"] = pd.to_numeric(arquivo_nuvem["Quantidade"])
+
+        cliente_desc = arquivo_nuvem.sort_values(by="Quantidade", ascending=False)
+
+        print("******************************************************************************")
+        print("*****                    Volume negociado por Cliente                    *****")
+        print("******************************************************************************")
+
+        for i, row in cliente_desc.iterrows():
+            print(f"{row['Cliente']} ---- {row['Porduto']} ---- {row['Quantidade']}")
+        print("-------------------------------------")
+        print()
+
+    except Exception as e:
+        print("Erro ao carregar dados:", e)
+
+if __name__ == "__main__":
+    main()
